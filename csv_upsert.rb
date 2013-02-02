35a34419d47b5ab8eac49ab27e2a945e7c02572a
Martin Vielsmaier <martin.vielsmaier@gmail.com>
2013-02-02 18:05:46 +0100

    Added csv import script for accounts

---
+++ csv_upsert.rb
@@ -0,0 +1,18 @@
+require 'csv'
+
+def import(file)
+  unmatched = []
+  CSV.foreach(file, headers: true) do |line|
+    account = Account.where(name: line['number']).first
+    if account
+      account.update_attributes(Hash[line.select { |k,_| %(name kind).include?(k) }])
+      puts account.to_s
+      p line
+    else
+      unmatched << line
+      account = Account.create!(Hash[line.select { |k,_| %(number name kind).include?(k) }])
+      account.update_attribute :member_account, true
+    end
+  end
+  unmatched
+end
